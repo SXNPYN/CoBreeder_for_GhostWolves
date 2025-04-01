519abc478ccac71a68a5d0b10312393b13b6ee25
SXNPYN <payne.saxen@gmail.com>
2025-04-01 14:58:58 +0100

    Add ability to generate random data

--- tests/data-generation.py
+++ tests/data-generation.py
@@ -4,14 +4,14 @@ import numpy as np
 import os
 
 NUM_INDIVIDUALS = 50
+NUM_GROUPS = 10
 MIN_ALLELES = 150
 MAX_ALLELES = 900
-NUM_GROUPS = 10
 MIN_PR = 1
 MAX_PR = 1000
 
 
-# Generate groups
+# Generate groups.
 groups = pd.DataFrame(columns=["ID", "MinSize", "MaxSize", "NumMale", "NumFemale", "PRThreshold"])
 groups['ID'] = [i for i in range(NUM_GROUPS)]
 groups['MinSize'] = [2]*NUM_GROUPS
@@ -21,7 +21,7 @@ groups['NumFemale'] = [1]*NUM_GROUPS
 groups['PRThreshold'] = [-1]*NUM_GROUPS
 print(f'\n{groups.head()}')
 
-# Generate individuals
+# Generate individuals.
 individuals = pd.DataFrame(columns=["Name", "Male", "Female", "AssignToFirstGroup", "Alleles", "Proven", "Priority"])
 individuals['Name'] = [f"Individual_{i+1}" for i in range(NUM_INDIVIDUALS)]
 individuals['Male'] = [random.choice([0, 1]) for _ in range(NUM_INDIVIDUALS)]
@@ -32,7 +32,7 @@ individuals['Proven'] = [random.choice([0, 1]) for _ in range(NUM_INDIVIDUALS)]
 individuals['Priority'] = 0
 print(f'\n{individuals.head()}')
 
-# Generate PR matrix
+# Generate PR matrix.
 random_matrix = np.random.randint(MIN_PR, MAX_PR, size=(NUM_INDIVIDUALS, NUM_INDIVIDUALS))
 random_matrix = (random_matrix + random_matrix.T) / 2
 np.fill_diagonal(random_matrix, 0)
@@ -40,10 +40,10 @@ pr = pd.DataFrame(random_matrix, columns=individuals['Name'])
 pr = pr.astype(int)
 print(f'\n{pr.head()}')
 
-# Save data
+# Save data.
 data_dir_path = "data/wolves_randomly_generated"
 os.makedirs(data_dir_path, exist_ok=True)
-groups.to_csv(os.path.join("../", data_dir_path, "generated-individuals.csv"), index=False)
-individuals.to_csv(os.path.join("../", data_dir_path, "generated-groups.csv"), index=False)
+groups.to_csv(os.path.join("../", data_dir_path, "generated-groups.csv"), index=False)
+individuals.to_csv(os.path.join("../", data_dir_path, "generated-individuals.csv"), index=False)
 pr.to_csv(os.path.join("../", data_dir_path, "generated-pr-scaled.csv"), index=False)
 print("Random data generated in data/wolves_randomly_generated.")
