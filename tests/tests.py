f3badc9899f4377c011a459459960609c699c3b4
SXNPYN <payne.saxen@gmail.com>
2025-05-02 11:31:16 +0100

    Change filenames

--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,3 @@
 import unittest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../cobreeder_for_ghost_wolves')))
-import ghost_cobreeder_v2_CLI as cli
-
-
-class TestArgparse(unittest.TestCase):
-    def test_main_normal(self, mock_print):
-        cli.main()
-        mock_print.assert_called_once_with("Hello, Charlie!")
-
-    def test_main_shout(self, mock_print):
-        cli.main()
-        mock_print.assert_called_once_with("HELLO, DANA!")
-
-if __name__ == "__main__":
-    unittest.main()
